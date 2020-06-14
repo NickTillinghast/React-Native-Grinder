1bc59a9cd2fea9bc1cceb0d28eac96fb7d457780
Nick Tillinghast <nickt2@cox.net>
2020-06-14 09:10:31 -0700

    number 66

---
+++ src/screens/TextScreen.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TextInput } from 'react-native';
+
+
+const TextScreen = () => {
+    const [password, setPassword] = useState('');
+
+    return (
+        <View>
+            <Text>Enter Password</Text>
+            <TextInput
+                style={styles.input}
+                autoCapitalize="none"
+                autoCorrect={false}
+                value={password}
+                onChangeText={(newValue) => setPassword(newValue)}
+            />
+            {password.length < 4 ? <Text>Password must be 4 characters</Text> : null}
+        </View>
+    )
+
+}
+
+const styles = StyleSheet.create({
+    input: {
+        margin: 15,
+        borderColor: 'black',
+        borderWidth: 1,
+        padding: 20,
+        fontSize: 15
+    }
+});
+
+export default TextScreen;
